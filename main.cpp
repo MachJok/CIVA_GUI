4db2f88cb7a55f60274f4e2f3903188259113a9c
MachJok <mikevivaldi@gmail.com>
2021-09-23 12:04:09 -0700

    incorporated jsnapp changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -202,7 +202,7 @@ XPLMCursorStatus	dummy_cursor_status_handler(XPLMWindowID in_window_id, int x, i
 int					dummy_wheel_handler(XPLMWindowID in_window_id, int x, int y, int wheel, int clicks, void * in_refcon);
 void				dummy_key_handler(XPLMWindowID in_window_id, char key, XPLMKeyFlags flags, char virtual_key, void * in_refcon, int losing_focus);
 int					coord_in_rect(float x, float y, float * bounds_lbrt);
-void 				window_params_setup(XPLMCreateWindow_t param);
+void 				window_params_setup(XPLMCreateWindow_t *param);
 void 				menu_handler(void *, void *);
 
 int 				render_loop(XPLMDrawingPhase inPhase, int inIsBefore, void* refcon);
@@ -242,6 +242,11 @@ PLUGIN_API int XPluginStart(
 
 	image_path_setup();
 	init_font();
+
+
+							 
+
+ 
 	glewInit();
 	g_menu_container_idx = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "CIV-A Menu", 0, 0);
 	g_menu_id = XPLMCreateMenu("CIV-A Menu", XPLMFindPluginsMenu(), g_menu_container_idx, menu_handler, NULL);
@@ -258,7 +263,7 @@ PLUGIN_API int XPluginStart(
 		XPLMAppendMenuItemWithCommand(aircraft_menu, "Toggle Settings (Command-Based)", XPLMFindCommand("sim/operation/toggle_settings_window"));
 	}
 
-	window_params_setup(params);
+	window_params_setup(&params);
 	
 	for(int i = 0; i < 3; ++i) //replace with NUM_IRU
 	{
@@ -650,34 +655,34 @@ int dummy_wheel_handler(XPLMWindowID in_window_id, int x, int y, int wheel, int 
 	return 0;
 }
 //these are the window setup parameters
-void window_params_setup(XPLMCreateWindow_t param)
+void window_params_setup(XPLMCreateWindow_t *param)
 {
-	param.structSize = sizeof(param);
-	param.visible = 1;
-	param.drawWindowFunc = draw;
+	param->structSize = sizeof(XPLMCreateWindow_t);
+	param->visible = 1;
+	param->drawWindowFunc = draw;
 	// Note on "dummy" handlers:
 	// Even if we don't want to handle these events, we have to register a "do-nothing" callback for them
-	param.handleMouseClickFunc = dummy_mouse_handler;
-	param.handleRightClickFunc = dummy_mouse_handler;
-	param.handleMouseWheelFunc = dummy_wheel_handler;
-	param.handleKeyFunc = dummy_key_handler;
-	param.handleCursorFunc = dummy_cursor_status_handler;
-	param.refcon = NULL;
-	param.layer = xplm_WindowLayerFloatingWindows;
+	param->handleMouseClickFunc = dummy_mouse_handler;
+	param->handleRightClickFunc = dummy_mouse_handler;
+	param->handleMouseWheelFunc = dummy_wheel_handler;
+	param->handleKeyFunc = dummy_key_handler;
+	param->handleCursorFunc = dummy_cursor_status_handler;
+	param->refcon = NULL;
+	param->layer = xplm_WindowLayerFloatingWindows;
 	// Opt-in to styling our window like an X-Plane 11 native window
 	// If you're on XPLM300, not XPLM301, swap this enum for the literal value 1.
-	param.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
+	param->decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
 	
 	// Set the window's initial bounds
 	// Note that we're not guaranteed that the main monitor's lower left is at (0, 0)...
 	// We'll need to query for the global desktop bounds!
 	int left, bottom, right, top;
 	XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
-	param.left = left + 50;
-	param.bottom = bottom + 150;
-	param.right = param.left + MIN_WIDTH;
-	param.top = param.bottom + MIN_HEIGHT;
-	//REPLACE 3 WITH NUM_IRU
+	param->left = left + 50;
+	param->bottom = bottom + 150;
+	param->right = param->left + MIN_WIDTH;
+	param->top = param->bottom + MIN_HEIGHT;
+
 }
 
 void fini_cb()
